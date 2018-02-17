4e8878b8382a11bddc598b274ded214d1adcd1aa
afox@wework.com <afox@wework.com>
2018-02-17 18:44:19 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,9 @@
 # Write your #display_rainbow method here
+
+colors = (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
 def display_rainbow
-  
+  print colors  
+end
+
+display_rainbow(colors)
